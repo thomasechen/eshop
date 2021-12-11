a1c5946001cf0c0642b8d5ff996ff25f19df6d91
thomasechen <thomasechen@gmail.com>
2021-12-11 15:17:49 +0800

    github pages prepared

--- libs/orders/src/index.ts
+++ libs/orders/src/index.ts
@@ -3,5 +3,7 @@ export * from './lib/orders.module';
 export * from './lib/models/orders';
 export * from './lib/models/cart';
 export * from './lib/models/order-item';
+export * from '../src/lib/components/cart-icon/cart-icon.component';
+export * from '../src/lib/pages/cart-page/cart-page.component';
 export * from './lib/services/orders.service';
 export * from './lib/services/cart.service';
